652cb4ec3890933a78678187419bcbaabcb00d57
chenlei <dchenlei@yeah.net>
2020-03-20 15:54:16 +0800

    缓存穿透解决方案二种方式

--- server/src/main/java/com/weirdo/server/service/CacheFightService.java
+++ server/src/main/java/com/weirdo/server/service/CacheFightService.java
@@ -27,6 +27,7 @@ public class CacheFightService {
 
     private static final Logger log= LoggerFactory.getLogger(CacheFightService.class);
 
+    //雪花算法:SnowFlake算法生成id的结果是一个64bit大小的整数=41bit-时间戳+10bit-工作机器id+12bit-序列号
     private static final Snowflake SNOWFLAKE=new Snowflake(3,2);
 
     @Autowired
@@ -68,7 +69,12 @@ public class CacheFightService {
     }
 
 
-    //缓存穿透-查询商品-解决方法 设置key的value为""
+    /**
+     * 缓存穿透-查询商品-解决方法 设置key的value为""
+     * @param id
+     * @return
+     * @throws Exception
+     */
     public Item getItemV2(Integer id) throws Exception{
         Item item=null;
 
@@ -98,8 +104,12 @@ public class CacheFightService {
     }
 
 
-
-    //缓存穿透-查询商品-解决方法 - 限流
+    /**
+     * 缓存穿透-查询商品-解决方法 - 限流
+     * @param id
+     * @return
+     * @throws Exception
+     */
     public Item getItemV3(Integer id) throws Exception{
         Item item=null;
 
@@ -133,24 +143,42 @@ public class CacheFightService {
     private StringRedisTemplate stringRedisTemplate;
 
 
-    //TODO:缓存击穿-查询商品详情
+    /**
+     * 缓存击穿-查询商品详情
+     * Redis分布式锁
+     * @param id
+     * @return
+     * @throws Exception
+     */
     public Item getItemCacheBeat(Integer id) throws Exception{
         Item item=null;
 
         if (redisService.exist(id.toString())){
+            log.info("从Redis缓存中取数据");
             String result=redisService.get(id.toString()).toString();
 
             if (StrUtil.isNotBlank(result)){
                 item=objectMapper.readValue(result,Item.class);
             }
         }else{
+            /**
+             * Redis Setnx（SET if Not eXists） 命令在指定的 key 不存在时，为 key 设置指定的值
+             * Redis Expire 命令用于设置 key 的过期时间，key 过期后将不再可用。单位以秒计。
+             * Redis DEL 命令用于删除已存在的键。不存在的 key 会被忽略。
+             */
             //TODO:分布式锁的实现 - 同一时刻只能保证 拥有该key 的一个线程进入 执行共享的业务代码
             //TODO:SETNX、EXPIRE、DELETE ； Redis单线程（它的操作是原子性操作） - io多路复用（单核cpu 却支持多任务、多用户的使用，频繁切换很快，用户识别不了）
+
+            //雪花算法：获取不重复的ID
             String value=SNOWFLAKE.nextIdStr();
+
             ValueOperations<String,String> valueOperations=stringRedisTemplate.opsForValue();
+            //设置一个key，value，返回true/false
             Boolean lock=valueOperations.setIfAbsent(Constant.RedisCacheBeatLockKey,value);
             try {
                 if (lock){
+                    log.info("------------------进入到Redis分布式锁中------------------");
+                    //设置操作时间 key设置过期时间  等待时间
                     stringRedisTemplate.expire(Constant.RedisCacheBeatLockKey,10L,TimeUnit.SECONDS);
 
                     log.info("---缓存击穿，从数据库查询：id={}",id);
@@ -165,8 +193,10 @@ public class CacheFightService {
 
                 }
             }finally {
+                //从Redis缓存获取雪花算法的ID ，如果获取的ID不为null 并且 与 生成的ID相同，删除缓存中分布式锁ID
                 String currValue=valueOperations.get(Constant.RedisCacheBeatLockKey);
                 if (StrUtil.isNotBlank(currValue) && currValue.equals(value)){
+                    log.info("拿到数据删除Redis缓存分布式锁ID");
                     stringRedisTemplate.delete(Constant.RedisCacheBeatLockKey);
                 }
             }
